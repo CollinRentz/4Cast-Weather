3956aacb524d958b6936be93c2dbbbaff7a2b485
Collin Rentz <rentzcollin@gmail.com>
2023-03-03 14:30:17 -0700

    Initial Commit

---
+++ 4Cast Weather/_Cast_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Cast_WeatherApp.swift
+//  4Cast Weather
+//
+//  Created by Collin Rentz on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct _Cast_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
